1ed207bb987af4b24b97d4002148b1e1c3a7dcee
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-10 08:30:11 +0300

    add quote.component.spec.ts

--- src/app/quote/quote.component.spec.ts
+++ src/app/quote/quote.component.spec.ts
@@ -1,25 +1,25 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { QuoteComponent } from './quote.component';
+import { QuoteComponent } from './quote.component';
 
-// describe('QuoteComponent', () => {
-//   let component: QuoteComponent;
-//   let fixture: ComponentFixture<QuoteComponent>;
+describe('QuoteComponent', () => {
+  let component: QuoteComponent;
+  let fixture: ComponentFixture<QuoteComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ QuoteComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuoteComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(QuoteComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
